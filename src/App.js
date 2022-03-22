b48477c0f184bff6e76cb568157608aa81c0afdd
CristianR <cristiangraffo33@gmail.com>
2022-03-22 16:11:02 -0300

    initial setup & create screens

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+import Home from './screens/home';
+
+const App = () => {
+  return (
+      <Home />
+  );
+};
+
+export default App;
