1a90843f308aea137c41a14f95fbf8e22c56bb57
CristianR <cristiangraffo33@gmail.com>
2022-04-13 05:26:31 -0300

    add redux thunk & imp middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import CategoryReducer from './reducers/category.reducer';
 import ProductReducer from './reducers/product.reducer';
+import thunk from 'redux-thunk';
 
 const RootReducer = combineReducers({
   categories: CategoryReducer,
   products: ProductReducer,
 });
 
-export default createStore(RootReducer);
+export default createStore(RootReducer, applyMiddleware(thunk));
---
+++ src/utils/database.js
@@ -0,0 +1,1 @@
+export const URL_API = 'https://breadapp-a9253-default-rtdb.firebaseio.com/';
